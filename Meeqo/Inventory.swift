e3c187eeb213ef4f1949a8ceaef066aece6ee06c
Patrik <patrik@Patriks-Mac.local>
2015-04-17 10:10:44 -0700

    Inventory basics

---
+++ Meeqo/Inventory.swift
@@ -0,0 +1,13 @@
+//
+//  Inventory.swift
+//  Meeqo
+//
+//  Created by Patrik on 4/17/15.
+//  Copyright (c) 2015 MyITSolver. All rights reserved.
+//
+
+import Foundation
+
+public struct IceCream {
+    //static
+}
