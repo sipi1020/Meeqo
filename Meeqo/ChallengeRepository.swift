6b9c8dd8f685e3f6584b20065d0d6df25084bc01
sipi1020 <sipossipi1020@gmail.com>
2015-05-01 00:48:16 +0200

    Repo and manager extensions

---
+++ Meeqo/ChallengeRepository.swift
@@ -0,0 +1,13 @@
+//
+//  ChallengeRepository.swift
+//  Meeqo
+//
+//  Created by Livvit on 01/05/15.
+//  Copyright (c) 2015 MyITSolver. All rights reserved.
+//
+
+import Foundation
+
+class ChallengeRepository {
+    
+}
---
+++ Meeqo/MeeqoManager.swift
@@ -0,0 +1,23 @@
+//
+//  MeeqoManager.swift
+//  Meeqo
+//
+//  Created by Livvit on 01/05/15.
+//  Copyright (c) 2015 MyITSolver. All rights reserved.
+//
+
+import Foundation
+
+
+class MeeqoManager {
+    
+    static func buyMeeqo(color: NSString) {
+        switch color {
+            case "":
+                
+                break
+        default:
+                break
+        }
+    }
+}
